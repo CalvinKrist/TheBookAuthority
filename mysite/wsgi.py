0bd0b6f49a035a0d1af74656963fb25e02ac4d62
Calvin Krist <calvin.krist@yahoo.com>
2021-01-08 15:19:39 -0500

    Initial commit

---
+++ mysite/wsgi.py
@@ -0,0 +1,11 @@
+"""
+WSGI config for mysite project.
+It exposes the WSGI callable as a module-level variable named ``application``.
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
